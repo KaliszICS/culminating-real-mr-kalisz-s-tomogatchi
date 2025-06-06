532125786f678f65c1344d5a9cc3830b6dff9f9d
lily <lily.you@student.tdsb.on.ca>
2025-06-06 18:28:41 +0000

    T

--- src/main/java/Graphics.java
+++ src/main/java/Graphics.java
@@ -5,7 +5,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-//Sources used: https://www.geeksforgeeks.org/java-jframe/, https://blog.idrsolutions.com/how-to-read-png-files-in-java/ 
+//Sources used: https://www.geeksforgeeks.org/java-jframe/, https://blog.idrsolutions.com/how-to-read-png-files-in-java/ , https://medium.com/@michael71314/java-lesson-22-inserting-images-onto-the-jframe-a0a0b6540cca 
 public class Graphics {
     public void testPNG(){
         
