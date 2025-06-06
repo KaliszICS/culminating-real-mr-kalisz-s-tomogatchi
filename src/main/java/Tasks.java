0a41c2d5a9e13769875b3d82fd016a8069787040
Yu-Yen, Shen <yu-yen.shen@student.tdsb.on.ca>
2025-06-06 18:22:51 +0000

    yuyen

--- src/main/java/Tasks.java
+++ src/main/java/Tasks.java
@@ -2,9 +2,13 @@ abstract class Tasks{
     protected int energyCost;
     //abstract boolean checkAnswer();
 <<<<<<< HEAD
+<<<<<<< HEAD
     abstract void energyChange(MrKalisz kalisz); //need to be able to access his energy level each time we change the energy so we need to pass a kalisz object/class
 =======
     abstract int energyChange(MrKalisz kalisz); //need to be able to access his energy level each time we change the energy so we need to pass a kalisz object/class
+=======
+    abstract void energyChange(MrKalisz kalisz); //need to be able to access his energy level each time we change the energy so we need to pass a kalisz object/class
+>>>>>>> 588fcf6 (yuyen)
     abstract boolean checkAnswer();
 >>>>>>> d88aaa1 (yuyen)
 }
