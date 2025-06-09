f1f382e79572a0c423946df51bd889c3c314f274
Yu-Yen, Shen <yu-yen.shen@student.tdsb.on.ca>
2025-06-09 17:07:58 +0000

    yuyen

--- src/main/java/MrKalisz.java
+++ src/main/java/MrKalisz.java
@@ -3,21 +3,34 @@ public class MrKalisz{
     private String firstName;
     private String lastName;
 
+    /**
+     * constructor
+     */
     public MrKalisz(){
         this.energy = 50;
         this.firstName = "James";
         this.lastName = "Kalisz";
     }
 
-    //getters
+    /**
+     * get Mr. Kalisz's energy points
+     * @return Mr. Kalisz's energy remaining
+     */
     public int getEnergy(){
         return this.energy;
     }
 
-    //setters
+    /**
+     * set Mr. Kalisz's energy points
+     * @param energyCost the energy cost while doing a task
+     */
     public void setEnergy(int energyCost){
         this.energy = energyCost;
     }
+
+    /**
+     * @return name in the format of "Mr. Kalisz"
+     */
     @Override
     public String toString(){
         return "Mr." + this.lastName;
