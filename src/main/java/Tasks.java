b4693b4220e3dde571331fe5a23315e6e08a7da5
lily <lily.you@student.tdsb.on.ca>
2025-06-10 19:05:29 +0000

    hopefully like last commit

--- src/main/java/Tasks.java
+++ src/main/java/Tasks.java
@@ -1,17 +1,13 @@
 /**
- * 
+ * This class is the superclass of all the tasks in the game and contain the method that change the energy of the player.
  * @author Yaseman Nikoo
  * @author Lily You
  * @author Yu-Yen Shen
- * @author Jathave Puvirajan
+ * @author Jathav Puvirajan
  * @version 1.4 2025/06/11
  */
-/** 
- * This class is the sueprclass of all the tasks in the game and contain the method that change the energy of the player.
- */
+
 abstract class Tasks{
     protected int energyCost;
-    //abstract boolean checkAnswer();
     abstract void energyChange(MrKalisz kalisz); 
-    //need to be able to access his energy level each time we change the energy so we need to pass a kalisz object/class
 }
